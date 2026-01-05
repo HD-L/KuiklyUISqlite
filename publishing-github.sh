b6aad71e1d5b07013278abfb3c1ea07da1b640ab
HD-L <2728073932@qq.com>
2026-01-05 09:42:19 +0800

    调整发布脚本

--- publishing-github.sh
+++ publishing-github.sh
@@ -1,3 +1,3 @@
 ./gradlew -c settings.ohos.gradle.kts :kuiklySqlite:publish
 ./gradlew :kuiklySqlite-compiler:publish
-git subtree push --prefix ./maven-repo origin gh-pages
+git subtree push --prefix maven-repo origin gh-pages
